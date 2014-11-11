6db61fc302533e4eb757fd6137fb8bda3554a491
Pawel Cieslik <p.cieslik@macopedia.pl>
2014-11-11 12:20:40 +0100

    add some template

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
-/**
- * Created by cieslix on 10.11.14.
- */
-(function () {
-    console.log('app.run');
-})();
+// Closes the sidebar menu
+$("#menu-close").click(function (e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Scrolls to the selected menu item on the page
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+    $('[data-href]').each(function () {
+        $.ajax({
+            url: $(this).data('href'),
+            type: 'get',
+            success: function (data) {
+                $(data).insertBefore('footer');
+            }
+        });
+    });
+});
